ff733270169cdd062e13a56a8f0deeac02393483
taro shinjyuku <tarosjk@gmail.com>
2025-11-07 10:44:44 +0900

    Lesson4-05まで完了

--- lesson4/js/4-05-8.js
+++ lesson4/js/4-05-8.js
@@ -2,4 +2,8 @@ $( function(){
     $("#js-gnav-button").on("click", function(){
         $("#js-gnav").toggleClass("is-open");
     } );
+
+    $("#js-gnav-list").on("click", function(){
+        $("#js-gnav").removeClass("is-open");
+    });
 } );
